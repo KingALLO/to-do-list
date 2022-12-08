7a9c95b911ceeb2b7be6851fd6d579be1bd6da5a
Anil Karabulut <karabani@uni-bremen.de>
2022-12-08 23:31:01 +0100

    Top bar
    The style and arrangements of the top bar are implemented along with a clock, that shows the current time.

---
+++ clock.js
@@ -0,0 +1,20 @@
+function currentTime(){
+    let day = new Date();
+    let hour = day.getHours();
+    let min = day.getMinutes();
+    let sec = day.getSeconds();
+    
+    if(hour < 10){
+        hour = "0" + hour;
+    }
+    if(min < 10){
+        min = "0" + min;
+    }
+    if(sec < 10){
+        sec = "0" + sec;
+    }
+
+    let time = hour + ":" + min + ":" + sec;
+    document.getElementById("clock").innerHTML = time;
+}
+setInterval(currentTime, 1000);
