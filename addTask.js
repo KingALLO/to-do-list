da48ec52e924ac0c95f12c61ab441f55e209cec4
Anil Karabulut <karabani@uni-bremen.de>
2022-12-10 02:41:31 +0100

    Design and functions updated
    The Tasks Design and functionality of the add button have been improved.

--- addTask.js
+++ addTask.js
@@ -6,16 +6,26 @@ const taskInput = document.getElementById("newtask-input");
     });
 
     function addClick() {
+        // Check if the input box is empty
         if(taskInput.value.length) {
+        // Create a new task
         const newTask = document.createElement("div");
         newTask.classList.add("task");
         taskContainer.appendChild(newTask);
+
+        // Add the text value from the input box to the new task
         const newTaskName = document.createElement("p");
         newTaskName.classList.add("task-name");
         newTaskName.innerText = taskInput.value;
         newTask.appendChild(newTaskName);
-        console.log("add");
+
+        // Add a date to the new task
+        const newTaskDate = document.createElement("p");
+        newTaskDate.classList.add("task-date");
+        newTaskDate.innerText = new Date().toLocaleDateString();
+        newTask.appendChild(newTaskDate);
         } else {
+            // If the input box is empty, show an alert
             alert("You have to add a task first!");
         }
     }
