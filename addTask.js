c5fdcd09e093d532b185961ff55e4a7f5d44d762
Anil Karabulut <karabani@uni-bremen.de>
2022-12-10 21:21:25 +0100

    Updated Tasks
    Added new buttons to the tasks for deleting and editing a task and given new functionality to the button that adds a new task

--- addTask.js
+++ addTask.js
@@ -24,6 +24,31 @@ const taskInput = document.getElementById("newtask-input");
         newTaskDate.classList.add("task-date");
         newTaskDate.innerText = new Date().toLocaleDateString();
         newTask.appendChild(newTaskDate);
+
+        // Add the span, that contains the edit and delete buttons
+        const newButtonsSpan = document.createElement("span");
+        newButtonsSpan.classList.add("delete-and-edit");
+        
+        // Add an edit button to the span
+        const newTaskEdit = document.createElement("button");
+        newTaskEdit.classList.add("edit-button");
+        // Add an icon to the edit button
+        const newTaskEditIcon = document.createElement("i");
+        newTaskEditIcon.classList.add("fas", "fa-pencil");
+        newTaskEdit.appendChild(newTaskEditIcon);
+        newButtonsSpan.appendChild(newTaskEdit);
+
+        // Add a delete button to the span
+        const newTaskDelete = document.createElement("button");
+        newTaskDelete.classList.add("delete-button");
+        // Add an icon to the delete button
+        const newTaskDeleteIcon = document.createElement("i");
+        newTaskDelete.appendChild(newTaskDeleteIcon);
+        newTaskDeleteIcon.classList.add("fas", "fa-trash-alt");
+        newButtonsSpan.appendChild(newTaskDelete);
+
+        // Add the span to the new task
+        newTask.appendChild(newButtonsSpan);        
         } else {
             // If the input box is empty, show an alert
             alert("You have to add a task first!");
